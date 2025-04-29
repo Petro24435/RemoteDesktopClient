10188f0f88b068751380750f11fdb48d7338ffdc
Nazar <nazarii.antonyk.ki.2022@lpnu.ua>
2025-04-29 19:18:14 +0300

    Усі з'єднання через сервер ЧАСТИНА

---
+++ RemoteDesktopClient/serverUserRegistration.h
@@ -0,0 +1,21 @@
+#ifndef SERVERUSERREGISTRATION_H
+#define SERVERUSERREGISTRATION_H
+
+#include <string>
+
+// Структура конфігурації
+struct ServerConfig {
+    std::string server_ip = "192.168.0.200";   // IP сервера
+    int port = 8000;                        // Порт сервера
+
+    std::string GetBaseUrl() const {
+        return "http://" + server_ip + ":" + std::to_string(port);
+    }
+};
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output);
+
+// Оголошуємо глобальну статичну змінну, яка буде використовуватися для всієї програми
+extern ServerConfig globalConfig;
+
+#endif // SERVERUSERREGISTRATION_H
+
