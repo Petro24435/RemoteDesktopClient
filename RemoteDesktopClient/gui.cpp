415ebf9e22cfaaf86a5f47fc7924b5ab8d25f7ee
Nazar <nazarii.antonyk.ki.2022@lpnu.ua>
2025-05-02 23:18:18 +0300

    Оновлена версія логін gui

--- RemoteDesktopClient/gui.cpp
+++ RemoteDesktopClient/gui.cpp
@@ -5,36 +5,69 @@
 #include <string>
 #include "auth.h"
 #include "user.h"
+#include "main.h"
 #pragma comment(lib, "ws2_32.lib")
 
 
-
-// Визначаємо ці змінні
-HWND hUsernameEdit = NULL;
-HWND hPasswordEdit = NULL;
 extern bool loginSuccessful = false;
 extern Auth auth;
 extern HINSTANCE hInst;
 UserInfo currentUser;
+HWND hwndLabelUsername, hwndEditUsername;
+HWND hwndLabelPassword, hwndEditPassword;
+HWND hwndButtonLogin, hwndButtonRegister;
+HWND hwndLabelDevelopersInfo;
+static HBRUSH hWhiteBrush = CreateSolidBrush(RGB(255, 255, 255)); // білий фон
 // Прототипи функцій
 LRESULT CALLBACK WndProcLogin(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
 
 // Контроли логіну
 void CreateControls(HWND hwnd) {
-    CreateWindowEx(0, L"STATIC", L"Username:", WS_VISIBLE | WS_CHILD,
-        20, 50, 100, 25, hwnd, (HMENU)1, NULL, NULL);
-    hUsernameEdit = CreateWindowEx(0, L"EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
-        130, 50, 200, 25, hwnd, (HMENU)2, NULL, NULL);
+    //CreateWindowEx(0, L"STATIC", L"Username:", WS_VISIBLE | WS_CHILD,
+    //    20, 50, 100, 25, hwnd, (HMENU)1, NULL, NULL);
+    //hUsernameEdit = CreateWindowEx(0, L"EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
+    //    130, 50, 200, 25, hwnd, (HMENU)2, NULL, NULL);
 
-    CreateWindowEx(0, L"STATIC", L"Password:", WS_VISIBLE | WS_CHILD,
-        20, 90, 100, 25, hwnd, (HMENU)3, NULL, NULL);
-    hPasswordEdit = CreateWindowEx(0, L"EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER | ES_PASSWORD,
-        130, 90, 200, 25, hwnd, (HMENU)4, NULL, NULL);
+    //CreateWindowEx(0, L"STATIC", L"Password:", WS_VISIBLE | WS_CHILD,
+    //    20, 90, 100, 25, hwnd, (HMENU)3, NULL, NULL);
+    //hPasswordEdit = CreateWindowEx(0, L"EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER | ES_PASSWORD,
+    //    130, 90, 200, 25, hwnd, (HMENU)4, NULL, NULL);
 
-    CreateWindowEx(0, L"BUTTON", L"Login", WS_VISIBLE | WS_CHILD,
-        130, 130, 100, 30, hwnd, (HMENU)5, NULL, NULL);
-    CreateWindowEx(0, L"BUTTON", L"Register", WS_VISIBLE | WS_CHILD,
-        240, 130, 100, 30, hwnd, (HMENU)6, NULL, NULL);
+    //CreateWindowEx(0, L"BUTTON", L"Login", WS_VISIBLE | WS_CHILD,
+    //    130, 130, 100, 30, hwnd, (HMENU)5, NULL, NULL);
+    //CreateWindowEx(0, L"BUTTON", L"Register", WS_VISIBLE | WS_CHILD,
+    //    240, 130, 100, 30, hwnd, (HMENU)6, NULL, NULL);
+
+    CreateWindowEx(0, L"STATIC", L"Підключитися як:", WS_CHILD | WS_VISIBLE,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_LABEL_ROLE, NULL, NULL);
+
+    CreateWindowEx(0, L"BUTTON", L"Сервер", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON | WS_GROUP,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_RADIO_SERVER, NULL, NULL);
+
+    CreateWindowEx(0, L"BUTTON", L"Клієнт", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_RADIO_CLIENT, NULL, NULL);
+
+    hwndLabelUsername = CreateWindowEx(0, L"STATIC", L"Username:", WS_CHILD | WS_VISIBLE,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_LABEL_USERNAME, NULL, NULL);
+
+    hwndEditUsername = CreateWindowEx(0, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_EDIT_USERNAME, NULL, NULL);
+
+    hwndLabelPassword = CreateWindowEx(0, L"STATIC", L"Password:", WS_CHILD | WS_VISIBLE,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_LABEL_PASSWORD, NULL, NULL);
+
+    hwndEditPassword = CreateWindowEx(0, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_PASSWORD,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_EDIT_PASSWORD, NULL, NULL);
+
+    hwndButtonLogin = CreateWindowEx(0, L"BUTTON", L"Login", WS_CHILD | WS_VISIBLE,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_BUTTON_LOGIN, NULL, NULL);
+
+    hwndButtonRegister = CreateWindowEx(0, L"BUTTON", L"Register", WS_CHILD | WS_VISIBLE,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_BUTTON_REGISTER, NULL, NULL);
+
+    hwndLabelDevelopersInfo = CreateWindowEx(0, L"STATIC", L"Developers: Antonyk Nazarii, Solomchak Petro April - May 2025", WS_CHILD | WS_VISIBLE,
+        0, 0, 0, 0, hwnd, (HMENU)IDC_LABEL_DEVELOPERS_INFO, NULL, NULL);
+
 }
 // ФУНКЦІЯ ДЛЯ ОТРИМАННЯ IP, можливо потім треба буде змінити якщо не буде пахати ця. 18.04.2025 16:02
 std::string GetLocalIPAddress() {
@@ -66,31 +99,32 @@ LRESULT CALLBACK WndProcLogin(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     switch (msg) {
     case WM_CREATE:
         CreateControls(hwnd);
+        CheckRadioButton(hwnd, IDC_RADIO_SERVER, IDC_RADIO_CLIENT, IDC_RADIO_CLIENT); // за замовчуванням — клієнт
         break;
 
     case WM_COMMAND: {
-        if (LOWORD(wp) == 5 || LOWORD(wp) == 6) {
+        if (LOWORD(wp) == IDC_BUTTON_REGISTER || LOWORD(wp) == IDC_BUTTON_LOGIN) {
             char username[256], password[256];
-            GetWindowTextA(hUsernameEdit, username, sizeof(username));
-            GetWindowTextA(hPasswordEdit, password, sizeof(password));
+            GetWindowTextA(hwndEditUsername, username, sizeof(username));
+            GetWindowTextA(hwndEditPassword, password, sizeof(password));
 
             std::string strUsername(username);
             std::string strPassword(password);
 
-            if (LOWORD(wp) == 5) {  // Логін кнопка
+            if (LOWORD(wp) == IDC_BUTTON_LOGIN) {  // Логін кнопка
                 if (auth.Authenticate(strUsername, strPassword)) {
                     loginSuccessful = true;
                     currentUser.login = strUsername;
                     currentUser.password = strPassword;
                     currentUser.ip = GetLocalIPAddress();
 
-                    //MessageBox(hwnd, L"Login successful!", L"Success", MB_OK);
+                    MessageBox(hwnd, L"Login successful!", L"Success", MB_OK);
                 }
                 else {
                     MessageBox(hwnd, L"Invalid credentials!", L"Error", MB_OK | MB_ICONERROR);
                 }
             }
-            else if (LOWORD(wp) == 6) {
+            else if (LOWORD(wp) == IDC_BUTTON_REGISTER) {
                 if (auth.RegisterUser(strUsername, strPassword)) {
                     MessageBox(hwnd, L"Registration successful!", L"Success", MB_OK);
                 }
@@ -101,6 +135,79 @@ LRESULT CALLBACK WndProcLogin(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
         }
         break;
     }
+
+    case WM_SIZE:
+    {
+        int width = LOWORD(lp);
+        int height = HIWORD(lp);
+
+        // Обчислення масштабу
+        float scaleX = (float)width / UI_BASE_WIDTH;
+        float scaleY = (float)height / UI_BASE_HEIGHT;
+
+        auto sx = [&](int val) { return (int)(val * scaleX); };
+        auto sy = [&](int val) { return (int)(val * scaleY); };
+
+        MoveWindow(hwndLabelUsername, sx(UI_X_LABEL), sy(UI_Y_USER),
+            sx(UI_WIDTH_LABEL), sy(UI_HEIGHT_LINE), TRUE);
+
+        MoveWindow(hwndEditUsername, sx(UI_X_LABEL + UI_WIDTH_LABEL + 10), sy(UI_Y_USER),
+            sx(UI_WIDTH_EDIT), sy(UI_HEIGHT_LINE), TRUE);
+
+        MoveWindow(hwndLabelPassword, sx(UI_X_LABEL), sy(UI_Y_PASS),
+            sx(UI_WIDTH_LABEL), sy(UI_HEIGHT_LINE), TRUE);
+
+        MoveWindow(hwndEditPassword, sx(UI_X_LABEL + UI_WIDTH_LABEL + 10), sy(UI_Y_PASS),
+            sx(UI_WIDTH_EDIT), sy(UI_HEIGHT_LINE), TRUE);
+
+        MoveWindow(hwndButtonLogin, sx(UI_X_LABEL), sy(UI_Y_BUTTONS),
+            sx(UI_WIDTH_BUTTON), sy(UI_HEIGHT_BUTTON), TRUE);
+
+        MoveWindow(hwndButtonRegister, sx(UI_X_LABEL + UI_WIDTH_BUTTON + 20), sy(UI_Y_BUTTONS),
+            sx(UI_WIDTH_BUTTON), sy(UI_HEIGHT_BUTTON), TRUE);
+        
+        MoveWindow(hwndLabelDevelopersInfo, sx(20), sy(UI_BASE_HEIGHT - UI_HEIGHT_BUTTON - 20), sx(UI_BASE_WIDTH - 40), sy(UI_HEIGHT_BUTTON), TRUE);
+
+        MoveWindow(GetDlgItem(hwnd, IDC_LABEL_ROLE), sx(UI_X_LABEL), sy(5),
+            sx(UI_WIDTH_LABEL), sy(UI_HEIGHT_LINE), TRUE);
+
+        MoveWindow(GetDlgItem(hwnd, IDC_RADIO_SERVER), sx(UI_X_LABEL + UI_WIDTH_LABEL + 10), sy(5),
+            sx(80), sy(UI_HEIGHT_LINE), TRUE);
+
+        MoveWindow(GetDlgItem(hwnd, IDC_RADIO_CLIENT), sx(UI_X_LABEL + UI_WIDTH_LABEL + 100), sy(5),
+            sx(80), sy(UI_HEIGHT_LINE), TRUE);
+
+        if (hFont) DeleteObject(hFont);
+        int fontSize = min(sx(16), sy(16));
+        hFont = CreateFont(fontSize, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+            DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+            DEFAULT_PITCH | FF_DONTCARE, L"Segoe UI");
+
+        HWND controls[] = {
+            hwndLabelUsername, hwndEditUsername,
+            hwndLabelPassword, hwndEditPassword,
+            hwndButtonLogin, hwndButtonRegister,
+            hwndLabelDevelopersInfo,
+            GetDlgItem(hwnd, IDC_LABEL_ROLE),
+            GetDlgItem(hwnd, IDC_RADIO_SERVER),
+            GetDlgItem(hwnd, IDC_RADIO_CLIENT),
+
+        };
+
+        for (HWND ctrl : controls) {
+            SendMessage(ctrl, WM_SETFONT, (WPARAM)hFont, TRUE);
+        }
+        break;
+    }
+    case WM_CTLCOLORSTATIC:
+    case WM_CTLCOLORBTN:
+    {
+        HDC hdcStatic = (HDC)wp;
+        SetBkMode(hdcStatic, TRANSPARENT); // фон прозорий
+        SetBkColor(hdcStatic, RGB(255, 255, 255)); // білий фон
+        return (INT_PTR)hWhiteBrush;
+    }
+
 
     case WM_DESTROY:
         PostQuitMessage(0);
---
+++ RemoteDesktopClient/main.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define IDC_LABEL_USERNAME  1001
+#define IDC_EDIT_USERNAME   1002
+#define IDC_LABEL_PASSWORD  1003
+#define IDC_EDIT_PASSWORD   1004
+#define IDC_BUTTON_LOGIN    1005
+#define IDC_BUTTON_REGISTER   1006
+#define IDC_LABEL_DEVELOPERS_INFO 1007
+#define IDC_LABEL_ROLE       1008
+#define IDC_RADIO_SERVER     1009
+#define IDC_RADIO_CLIENT     1010
+
+
+// Базовий розмір вікна, для масштабування
+#define UI_BASE_WIDTH       400
+#define UI_BASE_HEIGHT      250
+
+// Початкові позиції і розміри (в пікселях при базовому масштабі)
+#define UI_X_LABEL          20
+#define UI_Y_USER           30
+#define UI_Y_PASS           70
+#define UI_Y_INFO           230
+#define UI_WIDTH_LABEL      100
+#define UI_WIDTH_EDIT       200
+#define UI_HEIGHT_LINE      25
+#define UI_Y_BUTTONS        120
+#define UI_WIDTH_BUTTON     80
+#define UI_HEIGHT_BUTTON    25
+HFONT hFont = NULL;
